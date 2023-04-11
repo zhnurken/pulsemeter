5882e3e4f933dfc134cd99197fecba3381a04623
Nurken Zhappargaly <zhnurken@gmail.com>
2023-04-11 16:39:48 +0600

    add smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -113,4 +113,36 @@ $(document).ready(function(){
         })
         return false;
       })
+
+      //Smooth scroll and pageup
+
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+      });
+
+      $("a").on("click", function (event) {
+        
+        if (this.hash !== "") {
+          
+          event.preventDefault();
+
+
+          const hash = this.hash;
+
+          $("html, body").animate(
+            {
+              scrollTop: $(hash).offset().top,
+            },
+            1000,
+            function () {
+
+              window.location.hash = hash;
+            }
+          );
+        } 
+      });
 });
