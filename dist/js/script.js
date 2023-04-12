7c396514137413a7c39af067e0b6a2a300b8aa47
Nurken Zhappargaly <zhnurken@gmail.com>
2023-04-12 07:45:32 +0600

    optimization

---
+++ dist/js/script.js
@@ -0,0 +1,150 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+      $("ul.catalog__tabs").on(
+        "click",
+        "li:not(.catalog__tab_active)",
+        function () {
+          $(this)
+            .addClass("catalog__tab_active")
+            .siblings()
+            .removeClass("catalog__tab_active")
+            .closest("div.container")
+            .find("div.catalog__content")
+            .removeClass("catalog__content_active")
+            .eq($(this).index())
+            .addClass("catalog__content_active");
+        }
+      );
+      
+
+      function toggleSlide (item) {
+              $(item).each(function (i) {
+                $(this).on("click", function (e) {
+                  e.preventDefault();
+                  $(".catalog-item__content")
+                    .eq(i)
+                    .toggleClass("catalog-item__content_active");
+                  $(".catalog-item__list")
+                    .eq(i)
+                    .toggleClass("catalog-item__list_active");
+                });
+              });
+      };
+
+      toggleSlide(".catalog-item__link");
+      toggleSlide(".catalog-item__back");
+
+      //Modals
+
+      $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn();
+      });
+
+
+      $(".modal__close").on("click", function () {
+        $(".overlay, #consultation, #order, #thanks").fadeOut();
+      });
+
+      $(".button_mini").each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $(".overlay, #order").fadeIn();
+        })
+      });
+
+      function valideForms(form) {
+        $(form).validate({
+            rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true,
+            },
+            phone: "required",
+            },
+            messages: {
+            name: "Пожалуйста введите свое имя",
+            phone: "Пожалуйста введите свой номер",
+            email: {
+                required: "Пожалуйста введите свою почту",
+                email: "Неправильно введен адрес почты",
+            },
+            },
+        });
+      }
+
+      valideForms("#consultation-form");
+      valideForms("#consultation form");
+      valideForms("#order form");
+
+      $('input[name=phone]').mask("+7 (999) 999-9999");
+
+      $('form').submit(function(e){
+        e.preventDefault();
+
+        if(!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $(".overlay, #thanks").fadeIn();
+
+            $("form").trigger("reset");
+        })
+        return false;
+      })
+
+      //Smooth scroll and pageup
+
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+      });
+
+      $("a").on("click", function (event) {
+        
+        if (this.hash !== "") {
+          
+          event.preventDefault();
+
+
+          const hash = this.hash;
+
+          $("html, body").animate(
+            {
+              scrollTop: $(hash).offset().top,
+            },
+            1000,
+            function () {
+
+              window.location.hash = hash;
+            }
+          );
+        } 
+      });
+
+      new WOW().init();
+});
